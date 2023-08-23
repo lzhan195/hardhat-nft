35a098eb07bad28617cc06bd8c8f170f4cab865c
lzhan195 <longtengzhang77@gmail.com>
2023-08-22 23:24:57 -0400

    local deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,57 +9,45 @@ const COINMARKEYCAP_API_KEY = process.env.COINMARKEYCAP_API_KEY
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    defaultNetwork: "hardhat",
-    networks: {
-        hardhat: {
-            chainId: 31337,
-            forking: {
-                url: MAINNET_RPC_URL,
-            },
-            blockConfirmations: 1,
-        },
-        sepolia: {
-            chainId: 11155111,
-            blockConfirmations: 6,
-            url: SEPOLIA_RPC_URL,
-            accounts: [PRIVATE_KEY],
-        },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
     },
-    etherscan: {
-        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-        apiKey: {
-            sepolia: ETHERSCAN_API_KEY,
-        },
-        customChains: [],
+    sepolia: {
+      chainId: 11155111,
+      blockConfirmations: 6,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
     },
-    gasReporter: {
-        enabled: false,
-        currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    customChains: [],
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  solidity: "0.8.7",
+  namedAccounts: {
+    deployer: {
+      default: 0,
     },
-    solidity: {
-        compilers: [
-            { version: "0.8.7" },
-            { version: "0.6.0" },
-            { version: "0.6.6" },
-            { version: "0.6.12" },
-            { version: "0.4.19" },
-        ],
+    player: {
+      default: 1,
     },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
-        player: {
-            default: 1,
-        },
-    },
-    mocha: {
-        timeout: 200000,
-    },
+  },
+  mocha: {
+    timeout: 200000,
+  }
 }
